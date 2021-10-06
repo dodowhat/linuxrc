e3f9f8f77f0e2b43380f3c25106e1d65c69e6a64
Eric Wen <dushu2048@gmail.com>
2021-10-06 23:01:54 +0800

    add app alias

---
+++ bashrc.d/app_alias.sh
@@ -0,0 +1,3 @@
+alias cfw='~/Portable/cfw/cfw > /dev/null 2>&1 & disown'
+alias telegram='~/Portable/Telegram/Telegram > /dev/null 2>&1 & disown'
+alias qbittorrent='~/Portable/qBittorrent/qBittorrent-Enhanced-Edition.AppImage > /dev/null 2>&1 & disown'
