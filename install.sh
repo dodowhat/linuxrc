79d41d448480ac74ac9aa8ccbf90f8bc456bb373
Eric Wen <dushu2048@gmail.com>
2021-10-21 20:07:23 +0800

    imporve installation script

--- install.sh
+++ install.sh
@@ -1,59 +1,98 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-dir=$(realpath $(dirname $0))
+dirname() {
+    # Usage: dirname "path"
+    local tmp=${1:-.}
 
-if [ "$1" = "bashrc.d" ]
-then
-    bashrc_src=$dir/bashrc.d
-    bashrc_dst=~/.bashrc.d
-    if [ ! -d $bashrc_dst ]; then
-        mkdir $bashrc_dst
-    fi
-    echo "cleaning $bashrc_dst ..."
-    rm $bashrc_dst/*
-    echo "linking $bashrc_src/* to $bashrc_dst/* ..."
-    for rc in $bashrc_src/*; do
-        if [ -f "$rc" ]; then
-            ln -s $rc $bashrc_dst/
+    [[ $tmp != *[!/]* ]] && {
+        printf '/\n'
+        return
+    }
+
+    tmp=${tmp%%"${tmp##*[!/]}"}
+
+    [[ $tmp != */* ]] && {
+        printf '.\n'
+        return
+    }
+
+    tmp=${tmp%/*}
+    tmp=${tmp%%"${tmp##*[!/]}"}
+
+    printf '%s\n' "${tmp:-/}"
+}
+
+dir=$(dirname $(realpath $0))
+
+case $1 in
+    "bashrc.d")
+        bashrc_src=$dir/bashrc.d
+        bashrc_dst=~/.bashrc.d
+        if [ ! -d $bashrc_dst ]; then
+            mkdir $bashrc_dst
         fi
-    done
-fi
 
-if [ "$1" = "i3" ]
-then
-    i3conf_src=$dir/i3.conf
-    i3conf_dst=~/.config/i3/config
-    echo "cleaning $i3conf_dst ..."
-    rm $i3conf_dst
-    echo "linking $i3conf_src to $i3conf_dst ..."
-    ln -s $i3conf_src $i3conf_dst
+        printf "cleaning $bashrc_dst ... "
+        rm $bashrc_dst/*
+        printf "success\n"
 
-    i3statusconf_src=$dir/i3status.conf
-    i3statusconf_dst=~/.config/i3status/config
-    if [ ! -f $i3statusconf_dst ]
-    then
-        mkdir -p $(dirname $i3statusconf_dst)
-    fi
-    echo "cleaning $i3statusconf_dst ..."
-    rm $i3statusconf_dst
-    echo "linking $i3statusconf_src to $i3statusconf_dst ..."
-    ln -s $i3statusconf_src $i3statusconf_dst
-fi
+        printf "creating symlink from $bashrc_src/* to $bashrc_dst/* ... "
+        for rc in $bashrc_src/*; do
+            if [ -f "$rc" ]; then
+                ln -s $rc $bashrc_dst/
+            fi
+        done
+        printf "success\n"
+    ;;
+    "i3")
+        i3conf_src=$dir/i3.conf
+        i3conf_dst=~/.config/i3/config
 
-if [ "$1" = "xresources" ]
-then
-    xresources_src=$dir/xresources
-    if [ "$2" = "4k" ]
-    then
-        xresources_dst=~/.xresources_4k
-    else
-        xresources_dst=~/.xresources
-    fi
-    echo "cleaning $xresources_dst ..."
-    rm $xresources_dst
-    echo "linking $xresources_src to $xresources_dst ..."
-    ln -s $xresources_src $xresources_dst
-    echo "applying $xresources_dst ..."
-    xrdb $xresources_dst
-fi
+        printf "cleaning $i3conf_dst ... "
+        rm $i3conf_dst
+        printf "success\n"
+
+        printf "creating symlink from $i3conf_src to $i3conf_dst ... "
+        ln -s $i3conf_src $i3conf_dst
+        printf "success\n"
+
+        i3statusconf_src=$dir/i3status.conf
+        i3statusconf_dst=~/.config/i3status/config
+        if [ ! -f $i3statusconf_dst ]; then
+            mkdir -p $(dirname $i3statusconf_dst)
+        fi
+
+        printf "cleaning $i3statusconf_dst ... "
+        rm $i3statusconf_dst
+        printf "success\n"
+
+        printf "creating symlink from $i3statusconf_src to $i3statusconf_dst ... "
+        ln -s $i3statusconf_src $i3statusconf_dst
+        printf "success\n"
+    ;;
+    "xresources")
+        xresources_src=$dir/xresources
+        if [ "$2" = "4k" ]
+        then
+            xresources_dst=~/.xresources_4k
+        else
+            xresources_dst=~/.xresources
+        fi
+
+        printf "cleaning $xresources_dst ... "
+        rm $xresources_dst
+        printf "success\n"
+
+        printf "creating symlink from $xresources_src to $xresources_dst ... "
+        ln -s $xresources_src $xresources_dst
+        printf "success\n"
+
+        printf "applying $xresources_dst ... "
+        xrdb $xresources_dst
+        printf "success\n"
+    ;;
+    *)
+        echo "usage: install.sh bashrc.d|i3|xresources"
+    ;;
+esac
 
